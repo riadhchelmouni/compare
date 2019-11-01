c2d1154e2d4ae9daefbb1990cf96bd1f22bc67bf
riadhchelmouni <dadi.holmes.007@gmail.com>
2019-11-01 08:15:10 +0100

    compare the two numbers

---
+++ Compare the two numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+int a,b;
+
+printf("Enter the Noumber a :");
+
+scanf("%d",&a);
+
+printf("Enter the Noumber b : ");
+scanf ("%d" ,&b);
+
+if(a>b)
+
+printf("The number a= %d bigger than b= %d\n",a,b);
+
+else if(a<b)
+
+printf("The number a = %d  smaller than b = %d\n",a,b);
+
+else
+
+printf(" the nmber a= %d equal b= %d\n",a,b);
+
+return 0;
+
+}
